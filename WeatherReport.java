d1e677b6f6172067d649dba96586abed30cd3157
jghoover <30665181+jghoover@users.noreply.github.com>
2017-08-21 12:52:39 -0500

    Add comments

--- WeatherReport.java
+++ WeatherReport.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.Scanner; //scanner utility provides the ability to read user input
 
 public class WeatherReport
 {
 
 	public static void main(String[] args) 
 	{
-		
+		//your code goes here
 	}
 
 }
